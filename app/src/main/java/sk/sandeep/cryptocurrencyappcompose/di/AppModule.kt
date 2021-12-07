19ae5c54468809b68d9b99d3327544e85d71ee58
sandeep <sksandeep9215@gmail.com>
2021-12-08 04:55:48 +0530

    add dagger setup

---
+++ app/src/main/java/sk/sandeep/cryptocurrencyappcompose/di/AppModule.kt
@@ -0,0 +1,35 @@
+package sk.sandeep.cryptocurrencyappcompose.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import sk.sandeep.cryptocurrencyappcompose.common.Constants.BASE_URL
+import sk.sandeep.cryptocurrencyappcompose.data.remote.CoinPaprikaApi
+import sk.sandeep.cryptocurrencyappcompose.data.repository.CoinRepositoryImpl
+import sk.sandeep.cryptocurrencyappcompose.domain.repository.CoinRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providePaprikaApi(): CoinPaprikaApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinPaprikaApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRepository(api: CoinPaprikaApi): CoinRepository {
+        return CoinRepositoryImpl(api)
+    }
+
+}
---
+++ app/src/main/java/sk/sandeep/cryptocurrencyappcompose/di/CoinApplication.kt
@@ -0,0 +1,8 @@
+package sk.sandeep.cryptocurrencyappcompose.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CoinApplication : Application() {
+}
