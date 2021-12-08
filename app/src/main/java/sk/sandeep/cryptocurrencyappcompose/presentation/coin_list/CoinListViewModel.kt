2cf2f0b832f27f1dbe64f7c2affa36b6b87c4cee
sandeep <sksandeep9215@gmail.com>
2021-12-08 08:07:42 +0530

    add CoinListViewModel.kt

---
+++ app/src/main/java/sk/sandeep/cryptocurrencyappcompose/presentation/coin_list/CoinListViewModel.kt
@@ -0,0 +1,39 @@
+package sk.sandeep.cryptocurrencyappcompose.presentation.coin_list
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.onEach
+import sk.sandeep.cryptocurrencyappcompose.common.Resource
+import sk.sandeep.cryptocurrencyappcompose.domain.use_case.get_coins.GetCoinsUseCase
+import javax.inject.Inject
+
+@HiltViewModel
+class CoinListViewModel @Inject constructor(
+    private val getCoinsUseCase: GetCoinsUseCase
+) : ViewModel() {
+    private val _state = mutableStateOf(CoinListState())
+    val state: State<CoinListState> = _state
+
+    private fun getCoins() {
+        getCoinsUseCase().onEach { result ->
+            when (result) {
+                is Resource.Success -> {
+                    _state.value = CoinListState(
+                        coins = result.data ?: emptyList()
+                    )
+                }
+                is Resource.Loading -> {
+                    _state.value = CoinListState(isLoading = true)
+                }
+                is Resource.Error -> {
+                    _state.value = CoinListState(
+                        error = result.message ?: "An unExpected Error Occurred"
+                    )
+                }
+            }
+
+        }
+    }
+}
